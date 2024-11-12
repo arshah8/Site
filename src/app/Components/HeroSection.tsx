7d5e8107fe3a86fc92b079464f43199147d7c05f
Abdur Rahman Shah <dragema44@gmail.com>
2024-11-13 01:05:04 +0500

    update

--- src/app/Components/HeroSection.tsx
+++ src/app/Components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export default function HeroSection() {
     return (
-      <div className="relative w-full h-[600px]  md:h-[500px] lg:h-[600px]">
+      <div className="relative w-full h-[400px]  md:h-[500px] lg:h-[600px]">
         <Image
           src="/erenjager.jpg"
           alt="ErenJager standing with two swords in his hands"
